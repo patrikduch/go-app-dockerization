b7224cd2aef5421e272404a5cd78290d2085e055
patrikduch <duchpatrik@icloud.com>
2022-11-17 11:29:53 +0100

    Environment variables

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go-app-dockerization
 Go Lang app dockerization example
+
+
+## Third party libraries
+
+go get github.com/joho/godotenv/cmd/godotenv
--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"example/todo-go/initializers"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+}
+
 func main() {
 	router := gin.Default() //new gin router initialization
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
 	router.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"data": "Hello World from Patrik Duch :)"})
 	}) // first endpoint returns Hello World
-	router.Run(":8000") //running application, Default port is 8080
+
+	router.Run() //running application, Default port is 8080
 }
